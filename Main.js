8c0dbb32e8bb8b8a78f76a76019f0d08c63b384f
JacobTyBruce <59230762+JacobTyBruce@users.noreply.github.com>
2020-04-30 03:49:45 -0400

    Slide Module Completed
    Added Counter at bottom. Ready For export. Can still make optimizations

--- Main.js
+++ Main.js
@@ -48,15 +48,24 @@ function pfpUnHover() {
   $("#personal-thumbnail").attr("src", "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1438993/PFPHoco.jpg")
 };
 
-// EXP
+// EXP --Go back and see if we can make currentSlide a reusable variable so no repeat
 var slideCtnr = $("#slideshow-ctnr");
 var slideItems = $("#slideshow-ctnr").children(".ss-obj");
 var slideLength = slideCtnr.children(".ss-obj").length;
+
+function updateSlide(pos) {
+  pos += 1;
+  $("#currentSlide").text(pos);
+}
+
 function slide_Start() {
   // init slideshow and hide all but first element
   for (let i = 1; i < slideItems.length;i++) {
     slideItems.eq(i).css("display", "none");
   };
+  $("#slideAmount").html(slideLength);
+  let currentSlide = slideCtnr.children(".ss-obj:visible");
+  updateSlide(currentSlide.index(".ss-obj"));
 };
 
 // when running if/else and calc index, used class arg and numerical exp so didnt calc btns, purely for clarity as btns are not a ssElem
@@ -71,6 +80,8 @@ function nextSlide() {
     slideItems.first().show();
     currentSlide.hide();
   };
+  currentSlide = slideCtnr.children(".ss-obj:visible");
+  updateSlide(currentSlide.index(".ss-obj"));
 };
 
 function lastSlide() {
@@ -83,6 +94,8 @@ function lastSlide() {
     slideItems.last().show();
     currentSlide.hide();
   };
+  currentSlide = slideCtnr.children(".ss-obj:visible");
+  updateSlide(currentSlide.index(".ss-obj"));
 }
 
 window.onload = slide_Start();
